a475e685d181b29ffb8aa3bddc82034351e418b7
HONDOH Atsushi <kagyuu@hondou.homedns.org>
2019-08-30 02:26:32 +0900

    add moveToId() extension

--- jquery.onepage-scroll.js
+++ jquery.onepage-scroll.js
@@ -1,7 +1,13 @@
 !function($){
 
+  var defaults = {
+    beforeMove: null,
+    afterMove: null,
+  };
+  
   $.fn.onepage_scroll = function(options){
     var el = $(this),
+        settings = $.extend({}, defaults, options),
         sections = $("section"),
         pos = 0,
         total = sections.length;
@@ -22,6 +28,23 @@
       el.transformPage();
     }
 
+    $.fn.moveToId = function(target_id) {
+      var continue_loop = true;
+      sections.each(function(sec_index, sec_element){
+        $(sec_element).children("div").each(function(div_index, div_element){
+          if(target_id == $(div_element).attr("id")) {
+            pos = sec_index;
+            continue_loop = false;
+            return false; // break-loop
+          }
+        });
+        // return true : continue-loop
+        // return false : break-loop
+        return continue_loop; 
+      });
+      el.transformPage();
+    }
+
     $.fn.transformPage = function() {
       if (pos < 0) {
         pos = 0;
@@ -29,6 +52,9 @@
       if (pos >= total) {
         pos = total - 1;
       }
+
+      if (typeof settings.beforeMove == 'function') settings.beforeMove(pos+1);
+
       sections.each(function(sec_index, sec_element){
         $(sec_element).children("div").each(function(div_index, div_element){
           if(pos == sec_index) {
@@ -38,6 +64,8 @@
           }
         });
       });
+
+      if (typeof settings.afterMove == 'function') settings.afterMove(pos+1);
     }
 
     return false;
